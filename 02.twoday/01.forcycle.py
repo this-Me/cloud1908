aa204de96ec5644206f7e9873fbb5e7e78921db0
hlions <hlions@163.com>
2019-11-19 09:14:03 +0800

    data struct

--- 02.twoday/01.forcycle.py
+++ 02.twoday/01.forcycle.py
@@ -25,9 +25,24 @@ for i in range(1, 101):
     if i % 2 == 0 and i % 3 == 0:
         print(i)
 # 如何书写一个死循环?;
-while True:
-    pass
+# while True:
+#     pass
 
 # break及continue与if判断加入到循环体中后可以实现循环的终止和中断;
+for i in range(100):
+    print(i) # - 23
+    if i == 23:
+        break
 
 
+while True:
+    variable = input("please input your word: ")
+    if variable == "bavdu":
+        break
+
+for i in range(1, 101):
+    for j in range(1, 101):
+        if j % 2 == 0:
+            continue
+        print(j)
+    print("i = ", i)
